39f3b2efb5af1628a808368e96333ada9f05fda2
Jack <j.griffithsrico@gmail.com>
2025-10-16 23:49:35 +0100

    First commit: Navbar of Krish Portfolio

---
+++ frontend/src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './styles/_global.scss'
+import Navbar from './components/Navbar'
+
+export default function App(){
+    return(
+        <Navbar/>
+    )
+}
---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from "react";
+import '../styles/components/_navbar.scss'
+
+export default function Navbar(){
+    return(
+        <section id="navbar">
+            <div className="nav">
+                <a href="home" className="nav-item">HOME</a>
+                <a href="about" className="nav-item">ABOUT</a>
+                <a href="showcase" className="nav-item">SHOWCASE</a>
+                <a href="bookings" className="nav-item">BOOKINGS</a>
+            </div>
+        </section>
+    )
+}
